d9e42acd784964c32a15fd334afe390e0abeb7e9
Jimmy Cook <wjimmycook@gmail.com>
2022-04-22 07:17:24 -0400

    Event listener native methods

--- android/src/main/java/com/codemotionapps/reactnativehelpscout/HelpScoutModule.java
+++ android/src/main/java/com/codemotionapps/reactnativehelpscout/HelpScoutModule.java
@@ -163,4 +163,14 @@ public class HelpScoutModule extends ReactContextBaseJavaModule {
 	public void clearFormPrefill() {
 	  Beacon.contactFormReset();
 	}
+	
+	@ReactMethod
+    	public void addListener(String eventName) {
+		// Keep: Required for RN built in Event Emitter Calls.
+    	}
+
+    	@ReactMethod
+    	public void removeListeners(Integer count) {
+		// Keep: Required for RN built in Event Emitter Calls.
+    	}
 }
